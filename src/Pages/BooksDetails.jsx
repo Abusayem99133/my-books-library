059b6207ae16e2d44b50ddd5e78475bc86a98b77
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-26 16:24:45 +0600

    book details  dynamic work done

--- src/Pages/BooksDetails.jsx
+++ src/Pages/BooksDetails.jsx
@@ -1,15 +1,70 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 const BooksDetails = () => {
   const bookDetails = useLoaderData();
   const { id } = useParams();
   const idInteser = parseInt(id);
   const bDetails = bookDetails.find((bDetails) => bDetails.id === idInteser);
+  const {
+    author,
+    bookName,
+    category,
+    image,
+    publisher,
+    rating,
+    review,
+    tags,
+
+    totalPages,
+    yearOfPublishing,
+  } = bDetails;
   console.log(bDetails);
   //   console.log(bookDetails);
   return (
     <div>
-      <h1>hi im in book details</h1>
+      <div className="hero min-h-screen ">
+        <div className="hero-content space-x-6 flex-col lg:flex-row">
+          <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl mb-4 font-bold">{bookName}</h1>
+            <span> By: {author}</span>
+            <hr className="mt-4 mb-4" />
+            <span className="">{category}</span>
+            <hr className="mt-4" />
+            <p className="py-6">
+              <span className="font-black">review</span> {review}
+            </p>
+
+            <div className="flex space-x-8 mt-6 mb-2">
+              <p className="font-black">Tag:</p>
+              <span
+                className="block text-xs font-medium tracking-widest uppercase text-[#23be0a] bg-[#f4fcf3]
+           px-4 py-2 rounded-full"
+              >
+                {tags[0]}
+              </span>
+              <span className="block text-xs font-medium tracking-widest uppercase text-[#23be0a] bg-[#f4fcf3] px-4 py-2 rounded-full">
+                {tags[1]}
+              </span>
+            </div>
+            <hr />
+            <div className="space-y-5 mt-4">
+              <p>Number of Pages: {totalPages}</p>
+              <p>Publisher: {publisher}</p>
+              <p>Year of Publishing: {yearOfPublishing}</p>
+              <p>Rating: {rating}</p>
+            </div>
+            <div className="space-x-4 mt-4">
+              <Link>
+                <button className="btn bg-lime-400 py-4 px-8">Read</button>
+              </Link>
+              <Link>
+                <button className="btn bg-lime-400 py-4 px-8">Wishlist</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
