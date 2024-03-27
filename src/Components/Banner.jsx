4cca79c55509d197b5e6f72a5c68d03cdca413c8
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-27 09:04:29 +0600

    banner section responsive done

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,20 +2,22 @@ import { Link } from "react-router-dom";
 import Image from "../assets/img/thedatingplaybook-608x1024-removebg-preview.png";
 const Banner = () => {
   return (
-    <div className="ml-4 w-72 md:hero space-x-12 rounded-3xl bg-base-200">
+    <div className="ml-12 w-72 md:hero space-x-12 rounded-3xl bg-base-200">
       <div className="md:hero-content flex-col md:flex-row-reverse ">
         <img src={Image} className="md:max-w-sm rounded-lg shadow-2xl" />
         <div className="">
-          <h1 className="mt-6 text-3xl md:text-5xl space-x-6 font-bold">
+          <h1 className="mt-6 p-4 md:p-0 text-3xl md:text-5xl space-x-6 font-bold">
             Books to freshen up <br /> your bookshelf
           </h1>
 
-          <Link
-            to="/listed"
-            className=" btn md:btn bg-lime-500 md:bg-lime-500 mt-4 text-white"
-          >
-            View The List
-          </Link>
+          <div className="p-4 md:p-0">
+            <Link
+              to="/listed"
+              className=" btn  md:btn bg-lime-500 md:bg-lime-500 mt-4 text-white"
+            >
+              View The List
+            </Link>
+          </div>
         </div>
       </div>
     </div>
