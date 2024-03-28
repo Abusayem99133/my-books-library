061868b0085351a0cdeace3ab19713571e91a815
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-28 19:40:38 +0600

    footer added

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Components/Routes/Navbar";
+import Footer from "../Components/Footer";
 // import ListedBooks from "../Pages/Home/ListedBooks";
 
 const MainLayouts = () => {
@@ -8,6 +9,9 @@ const MainLayouts = () => {
     <div className="md:ml-24 mr-24">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <div className="mt-44">
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
