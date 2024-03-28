2a8a87fbdbb9538d37e01d86f577915198a1b476
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-28 19:45:22 +0600

    false folder and commit file remove from my vs code

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Components/Routes/Navbar";
-import Footer from "../Components/Footer";
-// import ListedBooks from "../Pages/Home/ListedBooks";
 
 const MainLayouts = () => {
   return (
     <div className="md:ml-24 mr-24">
       <Navbar></Navbar>
       <Outlet></Outlet>
-      <div className="mt-44">
-        <Footer></Footer>
-      </div>
     </div>
   );
 };
